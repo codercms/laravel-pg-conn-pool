0944c74df4ad738a7ab0d5253d361830173c8cd4
coder <coder1994@gmail.com>
2020-01-06 01:29:24 +0300

    Added caching of PDO Prepared statements for SELECT queries for performance reason

--- src/Database/PooledPostgresConnection.php
+++ src/Database/PooledPostgresConnection.php
@@ -11,6 +11,13 @@ class PooledPostgresConnection extends PostgresConnection implements PooledConne
     private static int $id = 0;
     private int $instanceId;
 
+    /**
+     * Cache of prepared statements to reuse them for performance reason
+     *
+     * @var \PDOStatement[]
+     */
+    private array $statements = [];
+
     public function __construct($pdo, $database = '', $tablePrefix = '', array $config = [])
     {
         parent::__construct($pdo, $database, $tablePrefix, $config);
@@ -22,5 +29,49 @@ class PooledPostgresConnection extends PostgresConnection implements PooledConne
     public function getId(): int
     {
         return $this->instanceId;
+    }
+
+    public function disconnect(): void
+    {
+        parent::disconnect();
+
+        $this->statements = [];
+    }
+
+    /**
+     * Run a select statement against the database.
+     *
+     * @param  string  $query
+     * @param  array  $bindings
+     * @param  bool  $useReadPdo
+     * @return array
+     */
+    public function select($query, $bindings = [], $useReadPdo = true): array
+    {
+        return $this->run($query, $bindings, function ($query, $bindings) use ($useReadPdo) {
+            if ($this->pretending()) {
+                return [];
+            }
+
+            $queryHash = \md5($query);
+            $statement = $this->statements[$queryHash] ?? null;
+
+            if (null === $statement) {
+                // For select statements, we'll simply execute the query and return an array
+                // of the database result set. Each element in the array will be a single
+                // row from the database table, and will either be an array or objects.
+                $this->statements[$queryHash] = $statement = $this->prepared(
+                    $this
+                        ->getPdoForSelect($useReadPdo)
+                        ->prepare($query)
+                );
+            }
+
+            $this->bindValues($statement, $this->prepareBindings($bindings));
+
+            $statement->execute();
+
+            return $statement->fetchAll();
+        });
     }
 }
